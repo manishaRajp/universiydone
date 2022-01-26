a43dc4939f9e805db5b48336c669e0510f7074c9
kanthariyapriyanka <kanthariyapiyu810@gmail.com>
2022-01-26 15:31:19 +0530

    modify route

--- resources/views/backend/university/college/registration_mail.blade.php
+++ resources/views/backend/university/college/registration_mail.blade.php
@@ -1,18 +1,26 @@
 <html lang="en">
+
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
 </head>
+
 <body>
     <div class="card">
-        <div class="top"><span>Hello {{ $clg_mail['name']}}</span>
+        <div class="top"><span>Hello {{ $clg_mail['name'] }}</span>
             <div class="bottom">
-           <p> Your Registration Done </p>
-            <h5>Here Your Login Information</h5>
-            <h6>Email:-{{ $clg_mail['email']}}</h6>
-            <h6>Password:-{{ $clg_mail['password']}}</h6>
+                <p> Your Registration Done </p>
+                <h5>Here Your Login Information</h5>
+                <h6>Email:-{{ $clg_mail['email'] }}</h6>
+                <h6>Password:-{{ $clg_mail['password'] }}</h6>
+            </div>
+            <div class="wrapper">
+                <a href="{{ route('college.login')}}">Go click For Login!</a>
             </div>
         </div>
     </div>
 </body>
+
 </html>
